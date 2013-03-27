cb579db19b9d3837dc5ec4d3f9a5641ec7b70856
Dominic Eschbach <dominic.eschbach@web.de>
2013-03-27 11:40:38 +0100

    Initial commit.

---
+++ eclipse/de.htwg.seapal.person/project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "person"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean,
+    "com.google.inject" % "guice" % "3.0"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
