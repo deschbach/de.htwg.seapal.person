31e9ca990530b62a133755abe78e0d1a9ec535ef
Dominic Eschbach <dominic.eschbach@web.de>
2013-04-03 23:47:20 +0200

    Modified Build.scala.

--- project/Build.scala
+++ project/Build.scala
@@ -12,13 +12,34 @@ object ApplicationBuild extends Build {
     	javaJdbc,
     	javaEbean,
     	"com.google.inject" % "guice" % "3.0",
-    	"com.google.inject.extensions" % "guice-multibindings" % "3.0"
+    	"com.google.inject.extensions" % "guice-multibindings" % "3.0",
+    	"util" % "util_2.10" % "1.0-SNAPSHOT"
  	)
 
-  	val main = play.Project(appName, appVersion, appDependencies).settings( 
+  	val main = play.Project(appName, appVersion, appDependencies).settings(
+  	
+		// known as group id  	 
   		organization := "de.htwg.seapal", // group id...
-		crossPaths := false, // disable using the Scala version in output paths and artifacts
+  		
+  		// disable using the Scala version in output paths and artifacts
+		crossPaths := false,
+		
+		// add additional resovers
 		resolvers += "HTWG Resolver" at "http://lenny2.in.htwg-konstanz.de:8081/artifactory/libs-snapshot-local",
-		publishTo := Some("HTWG Publisher" at "http://lenny2.in.htwg-konstanz.de:8081/artifactory/libs-snapshot-local;build.timestamp=" + new java.util.Date().getTime)
+		
+		// add publishing target
+		publishTo := Some("HTWG Publisher" at "http://lenny2.in.htwg-konstanz.de:8081/artifactory/libs-snapshot-local;build.timestamp=" + new java.util.Date().getTime),
+		
+		// setup entry points for sonar code analyzer
+  		pomExtra :=
+	  	<build>
+	    	<sourceDirectory>app</sourceDirectory>
+	    	<testSourceDirectory>test</testSourceDirectory>
+	    	<resources>
+	      		<resource>
+	        		<directory>app</directory>
+	      		</resource>
+	    	</resources>
+	  	</build>
 	)
 }
