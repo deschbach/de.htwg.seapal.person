e3120229fdad3217fc587cd9bdd059d3b53967a4
Dominic Eschbach <dominic.eschbach@web.de>
2013-06-19 01:02:00 +0200

    fix

--- test/de/htwg/seapal/person/controllers/impl/PersonControllerTest.java
+++ test/de/htwg/seapal/person/controllers/impl/PersonControllerTest.java
@@ -4,6 +4,8 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import de.htwg.seapal.boat.controllers.IBoatController;
+import de.htwg.seapal.boat.models.IBoat;
 import de.htwg.seapal.person.controllers.IPersonController;
 import de.htwg.seapal.person.controllers.IPersonControllerTest;
 import de.htwg.seapal.person.database.IPersonDatabase;
@@ -20,6 +22,8 @@ public class PersonControllerTest extends IPersonControllerTest{
 				bind(IPerson.class).to(de.htwg.seapal.person.models.mock.Person.class);
 				bind(IPersonController.class).to(de.htwg.seapal.person.controllers.impl.PersonController.class);	
 				bind(IPersonDatabase.class).to(de.htwg.seapal.person.database.mock.PersonDatabase.class);
+				bind(IBoatController.class).to(de.htwg.seapal.boat.controllers.mock.BoatController.class);
+				bind(IBoat.class).to(de.htwg.seapal.boat.models.mock.Boat.class);
 			}
 		});
 		return tInj;
